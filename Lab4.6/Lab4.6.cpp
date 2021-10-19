a18dfa1a5441f20725bdfe040d25a03b97c872f7
KyrylHodlevskiy <kn2004k@gmail.com>
2021-10-19 09:23:18 +0300

    Добавьте файлы проекта.

---
+++ Lab4.6/Lab4.6.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	double P, S;
+	int i, j;
+	P = 1;
+	j = 1;
+	while (j <= 15)
+	{
+		S = 0;
+		i = j;
+		while (i <= j * j)
+
+		{
+			S += pow(i, 2) ;
+			i++;
+
+		}
+		P *= sqrt(S);
+		j++;
+
+	}
+
+	cout << P << endl;
+	return 0;
+}
